c821a6600d26f46a96831467f1cf9ee9bca6c26c
pawelrogulski <pawelrogulski@o2.pl>
2023-03-06 16:39:35 +0100

    SpringAngularWebsocket

---
+++ websocket-frontend/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import {Component, HostListener} from '@angular/core';
+import {MessageService} from './message.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'websocket-frontend';
+  input;
+  constructor(private messageService: MessageService) {}
+  sendMessage() {
+    if (this.input) {
+      this.messageService.sendMessage(this.input);
+      this.input = '';
+    }
+  }
+
+  @HostListener('document:keydown', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent) {
+    if (event.key === 'Enter') {
+      this.sendMessage();
+    }
+  }
+}
---
+++ websocket-frontend/src/app/message.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+declare var SockJS;
+declare var Stomp;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MessageService {
+
+  constructor() {
+    this.initializeWebSocketConnection();
+  }
+  public stompClient;
+  public msg = [];
+  initializeWebSocketConnection() {
+    const serverUrl = 'http://localhost:8080/socket';
+    console.log(serverUrl);
+    const ws = new SockJS(serverUrl);
+    this.stompClient = Stomp.over(ws);
+    const that = this;
+    this.stompClient.connect({}, function(frame) {
+      that.stompClient.subscribe('/message', (message) => {
+        if (message.body) {
+          that.msg.push(message.body);
+        }
+      });
+    });
+  }
+
+  sendMessage(message) {
+    this.stompClient.send('/app/send/message' , {}, message);
+  }
+}
